4f30f4026f9ba1e6ab4abb5b06bd403d519d6b51
cod3-craft3r <awsmarchies@gmail.com>
2023-07-29 21:36:04 +0530

    v1.0

---
+++ scripts/content-script.js
@@ -0,0 +1,23 @@
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  console.log(readingTime);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("text");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  console.log(heading);
+
+  (heading).insertAdjacentElement("afterend", badge);
+}
